d6335b7ab4d08f476aeeb31e42c19eb578c60cc3
Jacob Hunt <jhunt764@gmail.com>
2020-05-13 16:40:25 -0400

    commented out some faultycode

--- src/main/java/com/codebrew/auth/MongoDetailsService.java
+++ src/main/java/com/codebrew/auth/MongoDetailsService.java
@@ -1,53 +1,53 @@
-package com.codebrew.auth;
+// package com.codebrew.auth;
 
-import java.util.ArrayList;
-import java.util.List;
+// import java.util.ArrayList;
+// import java.util.List;
 
-import com.codebrew.repository.*;
+// import com.codebrew.repository.*;
 
 
-import org.apache.catalina.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
+// import org.apache.catalina.User;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.security.core.authority.SimpleGrantedAuthority;
+// import org.springframework.security.core.userdetails.UserDetails;
+// import org.springframework.security.core.userdetails.UserDetailsService;
+// import org.springframework.security.core.userdetails.UsernameNotFoundException;
+// import org.springframework.security.crypto.password.PasswordEncoder;
 
-public class MongoDetailsService implements UserDetailsService {
+// public class MongoDetailsService implements UserDetailsService {
 
-    @Autowired
-    private UserRepository userRepository;
+//     @Autowired
+//     private UserRepository userRepository;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+//     @Autowired
+//     private PasswordEncoder passwordEncoder;
 
-    @Override
-    public UserDetails loadUserByUsername(String username) {
-        org.springframework.boot.autoconfigure.security.SecurityProperties.User user = userRepository
-                .findByUsername(username);
-        if (user == null) {
-            throw new UsernameNotFoundException(username);
-        }
-        return new org.springframework.security.core.userdetails.User(user.getName(),
-                user.getPassword(),
-                getAuthorities());
-    }
+//     @Override
+//     public UserDetails loadUserByUsername(String username) {
+//         org.springframework.boot.autoconfigure.security.SecurityProperties.User user = userRepository
+//                 .findByUsername(username);
+//         if (user == null) {
+//             throw new UsernameNotFoundException(username);
+//         }
+//         return new org.springframework.security.core.userdetails.User(user.getName(),
+//                 user.getPassword(),
+//                 getAuthorities());
+//     }
 
-    public UserDetails Save(User newUser) {
-        newUser.setPassword(passwordEncoder.encode(newUser.getPassword()));
-        User savedUser = userRepository.save(newUser);
-        return new org.springframework.security.core.userdetails.User(savedUser.getUsername(), savedUser.getPassword(),
-                getAuthorities());
-    }
+//     public UserDetails Save(User newUser) {
+//         newUser.setPassword(passwordEncoder.encode(newUser.getPassword()));
+//         User savedUser = userRepository.save(newUser);
+//         return new org.springframework.security.core.userdetails.User(savedUser.getUsername(), savedUser.getPassword(),
+//                 getAuthorities());
+//     }
 
-    private List<SimpleGrantedAuthority> getAuthorities() {
-        List<SimpleGrantedAuthority> authList = new ArrayList<>();
-        authList.add(new SimpleGrantedAuthority("ROLE_USER"));
-        return authList;
-    }
+//     private List<SimpleGrantedAuthority> getAuthorities() {
+//         List<SimpleGrantedAuthority> authList = new ArrayList<>();
+//         authList.add(new SimpleGrantedAuthority("ROLE_USER"));
+//         return authList;
+//     }
 
-	public UserDetailsService passwordEncoder(PasswordEncoder passwordEncoder) {
-		return null;
-	}
-}
+// 	public UserDetailsService passwordEncoder(PasswordEncoder passwordEncoder) {
+// 		return null;
+// 	}
+// }
--- src/main/java/com/codebrew/auth/WebSecurityConfig.java
+++ src/main/java/com/codebrew/auth/WebSecurityConfig.java
@@ -1,36 +1,5 @@
 package com.codebrew.auth;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-@Configuration
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    private MongoDetailsService mongoDetailsService;
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(mongoDetailsService.passwordEncoder(passwordEncoder()));
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/", "/home", "/css/**").permitAll().anyRequest().authenticated().and()
-                .formLogin().loginPage("/login").permitAll().and().logout().permitAll();
-    }
+public class WebSecurityConfig {
+    
 }
