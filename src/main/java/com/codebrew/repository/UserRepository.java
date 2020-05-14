7b17d9644b3fa38a3ddd00e623ef4204b6879876
Jacob Hunt <jhunt764@gmail.com>
2020-05-13 21:34:21 -0400

    @Repository added to Dao files

--- src/main/java/com/codebrew/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.codebrew.repository;
-
-import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, Long> {
-	User findByUsername(String username);
-}
