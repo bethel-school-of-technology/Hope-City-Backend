52d5f4d3e8ad39d75d0be5a50eb20783665b5c12
UlmoLordofWater <wesley.duris@gmail.com>
2020-05-20 20:25:25 -0400

    changed userRepository

--- src/main/java/com/codebrew/repository/UsersRepository.java
+++ src/main/java/com/codebrew/repository/UsersRepository.java
@@ -4,5 +4,5 @@ import com.codebrew.models.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsersRepository extends JpaRepository<Users, Long> {
-    Users findByEmail(String username);
+    Users findByEmail(String email);
 }
