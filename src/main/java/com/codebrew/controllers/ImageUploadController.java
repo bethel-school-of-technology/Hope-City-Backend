c56ca0bf8c1c45184598c07bf2dcbc3834716e07
UlmoLordofWater <wesley.duris@gmail.com>
2020-05-28 18:23:16 -0400

    added cross origin value

--- src/main/java/com/codebrew/controllers/ImageUploadController.java
+++ src/main/java/com/codebrew/controllers/ImageUploadController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "*")
 @RequestMapping(path = "image")
 public class ImageUploadController {
     @Autowired
