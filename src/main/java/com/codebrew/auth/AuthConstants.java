fec2ae54b1ac9250aa835884dafe75a4a48aca73
Jacob Hunt <jhunt764@gmail.com>
2020-06-07 19:52:26 -0400

    added admin toggle route and updated the passwordr

--- src/main/java/com/codebrew/auth/AuthConstants.java
+++ src/main/java/com/codebrew/auth/AuthConstants.java
@@ -6,11 +6,13 @@ public class AuthConstants {
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/user";
+    public static final String DELETE = "/user/{id}";
     public static final String GET_ALL = "/user";
     public static final String GET_ONE = "/user/{email}";
     public static final String LOGIN = "/user/login";
     public static final String UPDATE = "/user/update/{id}";
     public static final String UPDATE_Password = "/user/update/password/{id}";
+    public static final String UPDATE_ROLE = "/user/role/{id}";
     public static final String CREATE_EVENT = "/events/create";
     public static final String UPDATE_EVENT = "/events/update/{id}";
     public static final String DELETE_EVENT = "/events/delete/{id}";
