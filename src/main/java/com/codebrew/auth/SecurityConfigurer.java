ba05da4eaf13ecfb99a3acc2cd04f80852cf6463
Jacob Hunt <jhunt764@gmail.com>
2020-06-20 15:38:42 -0400

    removed unused imports

--- src/main/java/com/codebrew/auth/SecurityConfigurer.java
+++ src/main/java/com/codebrew/auth/SecurityConfigurer.java
@@ -1,7 +1,5 @@
 package com.codebrew.auth;
 
-import java.beans.Customizer;
-import java.util.Arrays;
 
 import com.codebrew.service.MySQLUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,16 +9,12 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
+
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @EnableWebSecurity
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
